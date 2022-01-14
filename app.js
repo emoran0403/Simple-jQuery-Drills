9decf9e8cf9eac9c2503fcc0f794731696abd9b9
Eric Moran <emoran0403@gmail.com>
2022-01-14 17:06:28 -0500

    alert properly displays value when submit button is pressed

--- app.js
+++ app.js
@@ -4,23 +4,28 @@
 */
 
 $(document).ready(function () {
-    console.log('document has loaded');
+    
+    //console.log('document has loaded');
     /*
     3: In app.js and use jQuery to select the <input> element you just created by the id.
     >> $('#idNameHere')
     */
 
     /*
-    4: Add a click event handler to the <input> button that will alert a message in the browser when the button is clicked. (hint)
-    >> 
+    4: Add a click event handler to the <input> button that will alert a message in the browser when the button is clicked.
+    >> $('selector').click(function () {functionToRunOnClickGoesHere}),
     */
-    $('#btnSubmit').click(function () {
-        alert('The button has been clicked!');
-        console.log('button has been clicked');
-    })
 
+    $('#btnSubmit').click(function (event) {
+        event.preventDefault(); // This prevents the Submit button from refreshing the page!
+        /*
+        6: In app.js when the form is submitted, alert the value of the text input field.
+        */
 
-
+        var valueToAlert = $('#inputBox').val(); //This sets the valueToAlert value as the value of the textbox
+        alert(`Submitted! You entered: ${valueToAlert}`); // This alerts the value from the text box
+        console.log(valueToAlert); //for debugging
+    });
 
 
 
